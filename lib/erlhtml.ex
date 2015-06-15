0298b443c5152443f4ab73f110ee79ff73e14378
Booker C. Bense <bbense@gmail.com>
2015-06-15 11:39:52 -0700

    wip

---
+++ lib/erlhtml.ex
@@ -0,0 +1,9 @@
+defmodule ErlHTML do
+	@moduledoc """
+	An iex helper for finding the locally installed html for 
+	an erlang module and opening it in the default system browser. 
+	"""
+
+	
+	
+end
